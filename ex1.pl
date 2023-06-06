66fbdac70ceb6d11644cc42db5552b97e89fb645
gb <l48402@alunos.uevora.pt>
2023-06-06 15:46:20 +0100

    update

--- ex1.pl
+++ ex1.pl
@@ -71,7 +71,7 @@ back_f:- estado_inicial(E0), back(E0,A),sort(A, L),write(' '),escreve(L).
 
 back(e([],A),A).
   back(E,Sol):- sucessor(E,E1), inc, ve_restricoes(E1),
-                       forCheck(E1,E2),
+                       forCheck(E1,E2),  
                           back(E2,Sol).
 
 sucessor(e([v(N,D,V)|R],E),e(R,[v(N,D,V)|E])):- member(V,D).
